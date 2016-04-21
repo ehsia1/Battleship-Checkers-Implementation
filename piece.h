3a9604340eb201b8b5441fc5b083033e9f31a051
ehsia1 <echsia16@gmail.com>
2016-04-21 18:13:58 -0400

    piece header

---
+++ piece.h
@@ -0,0 +1,35 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <utility>
+#include "game.h"
+
+class Piece {
+public:
+
+	Piece() {}
+
+	virtual void move(int num, char direction) {
+		num = num;
+		direction = direction;
+	}
+
+	virtual int has_coord(Coord c) {
+		int first = c.first;
+		int second = c.second;
+		int third = first + second;
+		third = 1;
+		return third;
+	}
+
+	virtual void hit(int h) {
+		h = h;
+	}
+
+	virtual bool is_sunk() {
+		return true;
+	}
+
+};
+
+#endif
