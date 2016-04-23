fb8f98ddc53bbbdaabcad3d61b1d1eb7f698b928
Rohan Tilva <rtilva1@ugrad2.cs.jhu.edu>
2016-04-22 21:05:17 -0400

    updated versions of battleship.h and driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void placement() {
+void placement(BattleshipGame bsgame) {
 	string dif_ships[] = {"AICRAFT CARRIER", "BATTLESHIP", "CRUISER", "SUBMARINE", "DESTROYER"};
 	int lengths[] = {5, 4, 3, 3, 2};
 
@@ -16,21 +16,56 @@ void placement() {
 			cout << endl;
 			int first = userInput.at(0) - '0';
 			int second = userInput.at(1) - '0';
-			char direction = userInput.at(2);
+			char direction = toupper(userInput.at(2));
+			Coord tempCoord = make_pair(first, second);
+			Ship temp(lengths[i], tempCoord, direction); 
 
-			for (int j=0; j < i; j++) {
-				if (
-
+			for (vector<Ship>::iterator it = bsgame.fleet1.begin() ; it != bsgame.fleet1.end(); it++) {
+				int counter =0;
+				for (int j=0; j<lengths[i]; j++){
+					if (direction == 'H'){
+						tempCoord.second++;
+					}else if (direction == 'V'){
+						tempCoord.first++;
+					}
+					if (!(*it).has_coord(tempCoord))
+						okay = false;
+					}
 
 			}
-
-
 		}
-
+		bsgame.fleet1[i] = temp;
 	}
 
-}
+	for (int i=0; i<5;i++) {
+                bool okay = true;
+                while (okay) {
+                        cout << "PLAYER 2 PLACE " << dif_ships[i] << ":";
+                        cin >> userInput;
+                        cout << endl;
+                        int first = userInput.at(0) - '0';
+                        int second = userInput.at(1) - '0';
+                        char direction = toupper(userInput.at(2));
+                        Coord tempCoord = make_pair(first, second);
+                        Ship temp(lengths[i], tempCoord, direction); 
 
+                        for (vector<Ship>::iterator it = bsgame.fleet2.begin() ; it != bsgame.fleet2.end(); it++) {
+                                int counter =0;
+                                for (int j=0; j<lengths[i]; j++){
+                                        if (direction == 'H'){
+                                                tempCoord.second++;
+                                        }else if (direction == 'V'){
+                                                tempCoord.first++;
+                                        }
+                                        if (!(*it).has_coord(tempCoord))
+                                                okay = false;
+                                        }
+
+                        }
+                }
+                bsgame.fleet2[i] = temp;
+        }
+}
 
 int main() {
 
@@ -51,7 +86,38 @@ int main() {
 			break;
 		case '1':
 			BattleshipGame bsgame;
-			
+			placement(bsgame);
+			string input;
+			int count1=0;
+			while (count1==0) {
+				if (turn == false) {
+					cout << "PLAYER 1:";
+					cin >> input;
+					cout << endl;
+					if (input.at(0) == 'q' || input.at(0) == 'Q') {
+						exit();
+					}
+					int first = input.at(0) - '0';
+					int second = input.at(1) - '0';
+					Coord d = make_pair(first, second);
+					if (bsgame.attack_square(d) == RESULT_PLAYER1_WINS) {
+						count1++;
+					}
+				} else {
+					cout << "PLAYER 2:";
+                                        cin >> input;
+                                        cout << endl;
+					if (input.at(0) == 'q' || input.at(0) == 'Q') {
+                                                exit();
+                                        }
+                                        int first = input.at(0) - '0';
+                                        int second = input.at(1) - '0';
+                                        Coord d = make_pair(first, second);
+					if (bsgame.attack_square(d) == RESULT_PLAYER2_WINS) {
+                                                count1++;
+                                        } 
+				}
+			}
 			break;
 		case '2':
 			//
