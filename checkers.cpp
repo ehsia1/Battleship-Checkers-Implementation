794e4021c9ea1908324bb31aa27f94c8d5ffaf9e
SM 600.120 Sp16 <ipsm55@ugradx.cs.jhu.edu>
2016-04-29 02:51:24 -0400

    commented and working

--- checkers.cpp
+++ checkers.cpp
@@ -7,16 +7,15 @@
 #include "checkers.h"
 using namespace std;
 
-
-	CheckersGame::CheckersGame(): Game(), p1pieces(), p2pieces() {}
+	CheckersGame::CheckersGame(): Game(), p1pieces(), p2pieces() {} //constructor for checkers game
 
 	int CheckersGame::check_coord(Coord c, int whosePiece) { //check to see if piece is there
 		int counter = 0;
 		typedef vector<CPiece>::iterator iterator;
-		if (whosePiece == 0) {
+		if (whosePiece == 0) {	//whosepiece tell fxn whose pieces to check
 			for (iterator it = p1pieces.begin(); it != p1pieces.end(); it++) {
 				if (it->has_coord(c)) {
-					return counter;
+					return counter; //index of piece in vector is returned
 				}
 				counter++;
 			}
@@ -28,29 +27,29 @@ using namespace std;
 				counter++;
 			}
 		}
-		return -1;
+		return -1; //if no piece has coordinate, -1 is returned
 	}
 
-	int CheckersGame::can_move(Coord c, string direction, int index) { //checks if move is valid
+	int CheckersGame::can_move(Coord c, string direction, int index) { //checks if move from a coordinate in a direction is valid
 		char vert = direction.at(0);
-		if (vert != 'B' && vert != 'T'){
+		if (vert != 'B' && vert != 'T'){ //if direction input is bad
 			return 0;
 		}
 		char lat = direction.at(1);
 		if (lat != 'R' && lat != 'L'){
 			return 0;
 		}
-		if (turn == true) {
+		if (turn == true) { //player 2 turn cases
 			if (vert == 'B') {
 				if (lat == 'L') {
-					Coord d = make_pair(c.first-1, c.second+1);
-					if (d.first < 0 || d.second > 7){
+					Coord d = make_pair(c.first-1, c.second+1); //occupancy state of coordinate is checked
+					if (d.first < 0 || d.second > 7){ //move is off the board
 						return -1000;
 					}
-					if (check_coord(d,1)>-1){
+					if (check_coord(d,1)>-1){ //player two has a piece there
 						return -1000;
 					}
-					if (check_coord(d,0)>-1){
+					if (check_coord(d,0)>-1){ //player one has a piece there (leaves possibility of jumping available)
 						return -30;
 					}
 					return 30;
@@ -67,7 +66,7 @@ using namespace std;
 					}
 					return 40;
 				}
-			} else if (p2pieces[index].checkKing()) {
+			} else if (p2pieces[index].checkKing()) { //checks directions if piece is king
 				if (vert == 'T') {
 					if (lat == 'L') {
 						Coord d = make_pair(c.first-1, c.second-1);
@@ -96,7 +95,7 @@ using namespace std;
 					}
 				}
 			}
-		} else if (turn == false) {
+		} else if (turn == false) { //player 1 cases
 			if (vert == 'T') {
                                 if (lat == 'L') {
                                         Coord d = make_pair(c.first-1, c.second-1);
@@ -153,24 +152,24 @@ using namespace std;
                                 }
                         }
 		}
-		return -1000;
+		return -1000; //something invalid (ex. making a move only a king can make)
 	}
 	//can jump
-	int CheckersGame::can_jump(Coord d, string direction, int index) {
-		int dir = can_move(d, direction, index);
+	int CheckersGame::can_jump(Coord d, string direction, int index) { //checks to see if piece can jump in that direction
+		int dir = can_move(d, direction, index); //checks for occupant of square one increment away
 		int temp;
-		if (turn == false) {
-			if (dir == -1) { //tl
-				Coord e = make_pair(d.first-1, d.second-1);
+		if (turn == false) { //player 1 cases
+			if (dir == -1) { //player 2 piece occupies space in immediate direction so jump possibility should be checked - direction: TL
+				Coord e = make_pair(d.first-1, d.second-1); //coordinate is updated so move function can be called to check for jump location
 				temp = can_move(e, direction, index);
-				if (temp == -1000){
+				if (temp == -1000){ //jump location is invalid
 					return 0;
 				}
-				if (temp == 1) {
+				if (temp == 1) { //jump location is available
 					return 1;
 				}
 				return 0;
-			} else if (dir == -2) { //tr
+			} else if (dir == -2) { //direction: TR
 				Coord e = make_pair(d.first+1, d.second-1);
 				temp = can_move(e, direction, index);
 				if (temp == -1000){
@@ -180,7 +179,7 @@ using namespace std;
 					return 2;
 				}
 				return 0;
-			} else if (dir == -3) { //bl
+			} else if (dir == -3) { //direction: BL
 				Coord e = make_pair(d.first-1, d.second+1);
 				temp = can_move(e, direction, index);
 				if (temp == -1000){
@@ -190,7 +189,7 @@ using namespace std;
 					return 3;
 				}
 				return 0;
-			} else if (dir == -4) { //br
+			} else if (dir == -4) { //BR
 				Coord e = make_pair(d.first+1, d.second+1);
 				temp = can_move(e, direction, index);
 				if (temp == -1000){
@@ -202,7 +201,7 @@ using namespace std;
 				return 0;
 			}
 		} else if (turn == true) { //player 2
-			if (dir == -10) { //tl
+			if (dir == -10) { //TL
                                 Coord e = make_pair(d.first-1, d.second-1);
                                 temp = can_move(e, direction, index);
 				if (temp==-1000){
@@ -212,7 +211,7 @@ using namespace std;
                                         return -1;
                                 }
                                 return 0;
-                        } else if (dir == -20) { //tr
+                        } else if (dir == -20) { //TR
                                 Coord e = make_pair(d.first+1, d.second-1);
                                 temp = can_move(e, direction, index);
 				if (temp == -1000){
@@ -222,7 +221,7 @@ using namespace std;
                                         return -2;
                                 }
                                 return 0;
-                        } else if (dir == -30) { //bl
+                        } else if (dir == -30) { //BL
                                 Coord e = make_pair(d.first-1, d.second+1);
                                 temp = can_move(e, direction, index);
 				if (temp == -1000){
@@ -232,7 +231,7 @@ using namespace std;
                                         return -3;
                                 }
                                 return 0;
-                        } else if (dir == -40) { //br
+                        } else if (dir == -40) { //BR
                                 Coord e = make_pair(d.first+1, d.second+1);
                                 temp = can_move(e, direction, index);
 				if (temp == -1000){
@@ -247,14 +246,14 @@ using namespace std;
 		return 0;
 	}
 
-	bool CheckersGame::is_jump(){
+	bool CheckersGame::is_jump(){ //checks if jump is available for player
 		typedef vector<CPiece>::iterator iterator;
 		if (turn == false){
 			int count = 0;
-			for (iterator it = p1pieces.begin(); it != p1pieces.end(); it++){
+			for (iterator it = p1pieces.begin(); it != p1pieces.end(); it++){ //goes through vector of pieces
 				Coord tcd = it->get_coord();
 				if (can_jump(tcd,"TR",count)>0 || can_jump(tcd,"TL",count)>0 || can_jump(tcd,"BL",count)>0 || can_jump(tcd,"BR",count)>0){
-					return true;
+					return true; //jump possibility exists
 				}
 				count++;
 			}
@@ -269,60 +268,65 @@ using namespace std;
 				count++;
 			}
 		}
-		return false;
+		return false; //no jumps available
 	}
 
-	//attack square (move)
+	//attack square (move piece in a direction and check for jump)
 	GameResult CheckersGame::attack_square(Coord d, string direction) {
 		if (turn == false) { //p1piece
 			int index = check_coord(d, 0);
-			if (index >= 0) {
-				if (is_jump()){
+			if (index >= 0) { //makes sure there exists a piece with coordinate
+				if (is_jump()){ //forces user to enter jump if jump is available
 					if (can_jump(d, direction, index) == 0){
 						return RESULT_INVALID;
 					}
 				}
 				int mv = can_move(d, direction, index);
-				if (mv == -1000 || mv == 0){
+				if (mv == -1000 || mv == 0){ //if move in that direction is invalid
 					return RESULT_INVALID;//invalid move;
 				}
 				int jump = can_jump(d, direction, index);
-				bool crowned = true;
 				if (mv > 0) { //move it
-					if (p1pieces[index].move(mv,1)==1){
+					if (p1pieces[index].move(mv,1)==1){ //if player is eligible for crowning
 						p1pieces[index].makeKing();
+						cout<<"CROWNED"<<endl;
 					}
 				}
-				else while (jump > 0 && crowned) { //jump it
-					int oindex = -1;
-					if (p1pieces[index].move(jump,2)==1){
+				else if (jump > 0) { //jump it
+					int oindex = -1; //used later for knowing which opponents piece to be jumped
+					bool crowned = false; //used for JUMPED message suppresion later
+					if (p1pieces[index].move(jump,2)==1){ //piece that is jumping has it coordinates updated in piece.move function
 						p1pieces[index].makeKing();
-						crowned = false;
+						cout<<"CROWNED"<<endl;
+						crowned = true;
 					}
 
-					if (jump == 1) { //tl
-						oindex = check_coord(make_pair(d.first-1,d.second-1),1);
-						d.first-=2;
+					if (jump == 1) { //direction: TL
+						oindex = check_coord(make_pair(d.first-1,d.second-1),1); //if jump is possible, get vector index of opponents piece to be jumped
+						d.first-=2; //update
 						d.second-=2;
-					} else if (jump == 2) { //tr
+					} else if (jump == 2) { //TR
 						oindex = check_coord(make_pair(d.first+1,d.second-1),1);
 						d.first+=2;
 						d.second-=2;
-					} else if (jump == 3) { //bl
+					} else if (jump == 3) { //BL
 						oindex = check_coord(make_pair(d.first-1,d.second+1),1);
 						d.first-=2;
 						d.second+=2;
-					} else if (jump == 4) { //br
+					} else if (jump == 4) { //BR
 						oindex = check_coord(make_pair(d.first+1,d.second+1),1);
 						d.first+=2;
 						d.second+=2;
 					}
 					if (oindex > -1) {
-						p2pieces[oindex].jumped();
+						p2pieces[oindex].jumped(); //update coordinate of jumped piece
+						if (!crowned){ //only print JUMPED if piece was no just crowned
+							cout<<"JUMPED"<<endl;
+						}
 					}
 					//check for additional jumps
-					int cnt = 0;
-					vector<string> dir;
+					int cnt = 0; //tracks number of available jumps
+					vector<string> dir; //stores directions of available jumps
 					Coord p1temp = p1pieces[index].get_coord();
 					if (can_jump(p1temp, "TL", index)){
 						cnt++;
@@ -342,37 +346,15 @@ using namespace std;
 							dir.push_back("BR");
 						}
 					}
-					string ndir = "GJ";
 					if (cnt>=1){
-						dir[0] = ndir;
-						cout<<"PLAYER1:";
-						string in;
-						cin>>in;
-						cout<<endl;
-						Coord t = make_pair(in.at(0)-'0',in.at(1)-'0');
-						if (t != p1temp){
-							return RESULT_INVALID;
-						}
-						char third = toupper(in.at(2));
-						char fourth = toupper(in.at(3));
-						string ndir = {third, fourth};
-						bool check = false;
-						for (vector<string>::iterator it = dir.begin(); it != dir.end(); it++){
-							if (ndir.compare((*it)) == 0){
-								check = true;
-							}
-						}
-						if (check != true){
-							return RESULT_INVALID;
-						}
+						return RESULT_KEEP_PLAYING; //if more jumps available, main will repromt user because turn was not changed by toggle()
 					}
-					jump = can_jump(p1temp,ndir,index);
 				}
 				if (check_win()) {
 					cout << "PLAYER 1 WINS" << endl;
 					return RESULT_PLAYER1_WINS;
 				} else {
-					toggle();
+					toggle(); //change turn
 				}
 			}else{
 				return RESULT_INVALID;
@@ -390,18 +372,19 @@ using namespace std;
 					return RESULT_INVALID; //invalid move;
 				}
 				int jump = can_jump(d, direction, index);
-				bool crowned = true;
 			        if (mv > 0) { //move it
 					if (p2pieces[index].move(mv/10,1)==2){
 						p2pieces[index].makeKing();
-						crowned = false;
+						cout<<"CROWNED"<<endl;
 					}
                                 }
-				else while (jump < 0 && crowned) { //jump it
+				else if(jump < 0) { //jump it
                                         int oindex = -1;
+					bool crowned = false;
 					if (p2pieces[index].move(jump*-1,2)==2){
 						p2pieces[index].makeKing();
-						crowned = false;
+						cout<<"CROWNED"<<endl;
+						crowned = true;
 					}
 
                                         if (jump == -1) { //tl
@@ -423,6 +406,9 @@ using namespace std;
                                         }
 					if (oindex > -1) {
 						p1pieces[oindex].jumped();
+						if (!crowned){
+							cout<<"JUMPED"<<endl;
+						}
 					}
 					//check for additional jumps
 					int cnt = 0;
@@ -446,31 +432,9 @@ using namespace std;
 							dir.push_back("TR");
 						}
 					}
-					string ndir = "GJ";
 					if (cnt>=1){
-						dir[0] = ndir;
-						cout<<"PLAYER2:";
-						string in;
-						cin>>in;
-						cout<<endl;
-						Coord t = make_pair(in.at(0)-'0',in.at(1)-'0');
-						if (t != p2temp){
-							return RESULT_INVALID;
-						}
-						char third = toupper(in.at(2));
-						char fourth = toupper(in.at(3));
-						string ndir = {third, fourth};
-						bool check = false;
-						for (vector<string>::iterator it = dir.begin(); it != dir.end(); it++){
-							if (ndir.compare((*it)) == 0){
-								check = true;
-							}
-						}
-						if (check != true){
-							return RESULT_INVALID;
-						}
+						return RESULT_KEEP_PLAYING;
 					}
-					jump = can_jump(p2temp,ndir,index);
                                 }
 				if (check_win()) {
 					cout << "PLAYER 2 WINS" << endl;
@@ -487,8 +451,8 @@ using namespace std;
 	//check win
 	bool CheckersGame::check_win() {
 		typedef vector<CPiece>::iterator iterator;
-		int counter = 0;
-		if (turn == false) {
+		int counter = 0; //keeps track of number of opponets pieces that were removed from board
+		if (turn == false) { //pplayer 1 checks player 2 pieces
 			for (iterator it = p2pieces.begin(); it != p2pieces.end(); it++) {
 				if (it->check_jumped() == -1) {
 					counter++;
@@ -501,14 +465,14 @@ using namespace std;
 				}
 			}
 		}
-		if (counter == 12) {
+		if (counter == 12) { //if all pieces are off board (were jumped)
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	void CheckersGame::printp() {
+	void CheckersGame::printp() { //this function is commented out - it prints location and pieces state (-1 for off board)
 		typedef vector<CPiece>::iterator iterator;
 		cout<<"p1 pieces"<<endl;
 		for (iterator it = p1pieces.begin(); it != p1pieces.end(); it++) {
@@ -520,7 +484,7 @@ using namespace std;
 		}
 	}
 
-        CheckersBoard::CheckersBoard() {
+        CheckersBoard::CheckersBoard() { //board
                 for (int i = 0; i < 8; i++) {
                         for (int j = 0; j < 8; j++) {
                                 ckarray[i][j] = '-';
